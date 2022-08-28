477296c5196d8a0cb528ec885db0f606b37b7d4f
tigercxx <76241201+tigercxx@users.noreply.github.com>
2022-08-28 17:49:49 +0800

    display structure from db

--- projectcompliance/src/component/reactFlowDisplayOnly.jsx
+++ projectcompliance/src/component/reactFlowDisplayOnly.jsx
@@ -5,45 +5,78 @@ import ReactFlow, {
     applyNodeChanges,
 } from "react-flow-renderer";
 import { useNavigate } from "react-router-dom";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import fetch from "node-fetch";
 
+import { getStructureChecker } from "../graphql/queries";
 import CustomNode from "./customNode";
 import "./customNode.css";
+import initialNodes from "../data/nodes";
+import initialEdges from "../data/edges";
 
 const nodeTypes = { customNode: CustomNode };
+const promise = fetch("http://localhost:3000/structuralresult");
 
-function FlowDisplay(propNodes, propEdges) {
+function FlowDisplay(propNodes, propEdges, uuid) {
     const defaultEdgeOptions = { animated: true };
 
     //   const getFromDb = () = {
     //     //Code that gets from data base, returns nodes _+ edges object to use in useState
     // }
-    console.log(propNodes);
-    console.log(propEdges);
-    const [nodes, setNodes] = useState(propNodes.propNodes);
+    let data1;
+    let data2;
 
-    const [edges, setEdges] = useState(propNodes.propEdges);
+    async function getSC() {
+        const info = await API.graphql({
+            query: getStructureChecker,
+            variables: {
+                id: `${propNodes.uuid}`,
+            },
+        });
+        console.log(info);
+        console.log("above is info in getsc");
+        console.log(JSON.parse(info.data.getStructureChecker.input)["nodes"]);
+        console.log(JSON.parse(info.data.getStructureChecker.input)["edges"]);
+        data1 = JSON.parse(info.data.getStructureChecker.input)["nodes"];
+        data2 = JSON.parse(info.data.getStructureChecker.input)["edges"];
+        setNodes(data1);
+        setEdges(data2);
 
-    console.log("Nodes is this now");
-    console.log(nodes.propNodes);
+        return [
+            JSON.parse(info.data.getStructureChecker.input)["nodes"],
+            JSON.parse(info.data.getStructureChecker.input)["edges"],
+        ];
+    }
+    getSC();
 
-    //   const onNodesChange = useCallback(
+    // promise.then((data1) => console.log(data1));
+    // const nodes = propNodes.propNodes;
+    // const edges = propEdges.propEdges;
+
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
+
+    // const onNodesChange = useCallback(
     //     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
     //     [setNodes]
-    //   );
-    //   const onEdgesChange = useCallback(
+    // );
+    // const onEdgesChange = useCallback(
     //     (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
     //     [setEdges]
-    //   );
-    //   const onConnect = useCallback(
+    // );
+    // const onConnect = useCallback(
     //     (connection) => setEdges((eds) => addEdge(connection, eds)),
     //     [setEdges]
-    //   );
+    // );
 
     return (
         <Fragment>
             <ReactFlow
                 nodes={nodes}
                 edges={edges}
+                // onNodesChange={onNodesChange}
+                // onEdgesChange={onEdgesChange}
+                // onConnect={onConnect}
                 defaultEdgeOptions={defaultEdgeOptions}
                 style={{ width: "100%", height: "90vh" }}
                 nodeTypes={nodeTypes}
--- projectcompliance/src/page/StructuralResult.js
+++ projectcompliance/src/page/StructuralResult.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { useNavigate, useParams, useLocation } from "react-router-dom";
 
-import FlowDisplay from '../component/reactFlowDisplayOnly';
+import FlowDisplay from "../component/reactFlowDisplayOnly";
 import nodes from "../data/nodes";
 
-
-function StructuralResult() {  
+function StructuralResult() {
     const location = useLocation();
     const nodesData = location.state?.nodesData;
     const edgesData = location.state?.edgesData;
-    
+    const uuid = location.state?.uuid;
+
     return (
         <div>
             <h1>Result Page</h1>
             <h1></h1>
             <h2> Recommendations & Review</h2>
-            <FlowDisplay propNodes={nodesData} propEdges={edgesData}/>
-
+            <FlowDisplay
+                propNodes={nodesData}
+                propEdges={edgesData}
+                uuid={uuid}
+            />
         </div>
     );
 }

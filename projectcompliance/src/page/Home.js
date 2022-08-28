3d7696b83fe6a53235e9c06b5a6c1c513122d22f
Xun Yi <xunyi.work@gmail.com>
2022-08-28 17:26:30 +0800

    Done!!!!

--- projectcompliance/src/page/Home.js
+++ projectcompliance/src/page/Home.js
@@ -4,19 +4,20 @@ import HomeButtons from "../component/homeButtons";
 
 import {Container, Row, Col} from 'react-bootstrap';
 import splashImage from "../images/splash.jpg";
-
+import Footer from "../component/footer";
 
 function Home() {
     return(
             <div>
                 <Row className="justify-content-center banner">
-                    <h1 className="text-left ms-5 mt-3">XADEX</h1>
+                    <h1 className="text-left homeTitle">XADEX</h1>
                 </Row>
                 <Row className="text-center mt-4 mb-4">
                     <p className="bigText">Say Hello.</p>
                     <p className="homeText">to a new and easy way to succeed in the financial world.</p>
                 </Row>
                 <HomeButtons/>
+            
             </div>
        
     )

8db17c60576fabeaea031b8ccaa552665f140fde
Xun Yi <xunyi.work@gmail.com>
2022-08-28 16:17:46 +0800

    Additional work

--- projectcompliance/src/component/navBar.jsx
+++ projectcompliance/src/component/navBar.jsx
@@ -20,24 +20,40 @@ function NavBar() {
 
 
     return (
-        
         <div className="NavBar">
-            <div className="justify-items-center" > 
+            <Row className="justify-content-end banner">
+                <Col lg={1}>
+                    <p className="h1Clone ">XADEX</p>  
+                </Col>
+              
+                <Col lg={10}>
+                    
+                    <Button variant="dark" className="NavText ms-5 mt-4 px-4" onClick={linkToHome}>HOME</Button>
+                    <Button variant="dark" className="NavText ms-5 mt-4 px-4" onClick={linkToComplianceChecker}>COMPLIANCE CHECKER</Button>
+                    <Button variant="dark" className="NavText ms-5 mt-4 px-4" onClick={linkToStructuralChecker}>STRUCTURAL CHECKER</Button>
+              
+                </Col>
+            </Row>
+        </div>
+
+
+        // <div className="NavBar">
+        //     <div className="justify-items-center" > 
                            
-                <Row>
-                    <Col lg={1}>
-                        <h1 >XADEX</h1>
-                    </Col>
-                    <Col lg={6}>
-                    <Button variant="dark" className="NavText" onClick={linkToHome}>HOME</Button>
-                    <Button variant="dark" className="NavText" onClick={linkToComplianceChecker}>COMPLIANCE CHECKER</Button>
-                    <Button variant="dark" className="NavText" onClick={linkToStructuralChecker}>STRUCTURAL CHECKER</Button>
-                    </Col>
-                </Row>
+        //         <Row>
+        //             <Col lg={1}>
+        //                 <h1 >XADEX</h1>
+        //             </Col>
+        //             <Col lg={6}>
+        //                 <Button variant="dark" className="NavText" onClick={linkToHome}>HOME</Button>
+        //                 <Button variant="dark" className="NavText" onClick={linkToComplianceChecker}>COMPLIANCE CHECKER</Button>
+        //                 <Button variant="dark" className="NavText" onClick={linkToStructuralChecker}>STRUCTURAL CHECKER</Button>
+        //             </Col>
+        //         </Row>
             
 
-            </div>
-        </div>
+        //     </div>
+        // </div>
      
     )
 };
